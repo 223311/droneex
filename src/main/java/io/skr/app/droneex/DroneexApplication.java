370c120ad18406e516c12c825d44a8897345a6b0
Sankar <panda.bhabani31@gmail.com>
2020-07-07 19:59:06 +0530

    Update DroneexApplication.java

--- src/main/java/io/skr/app/droneex/DroneexApplication.java
+++ src/main/java/io/skr/app/droneex/DroneexApplication.java
@@ -18,6 +18,6 @@ public class DroneexApplication {
 class HelloController{
 	@GetMapping("/hello")
 	public String sayHello(){
-		return "Hello Guest!!! v23";
+		return "Hello Guest!!! v23"gfsdjghjsjjsdgjg;
 	}
 }
